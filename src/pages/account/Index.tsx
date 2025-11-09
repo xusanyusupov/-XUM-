de50272cc74d00faacfaf926a3a48915fd70528b
xusanyusupov <yusupov.xusan.org@gmail.com>
2025-11-10 00:00:13 +0500

    test

--- src/pages/account/Index.tsx
+++ src/pages/account/Index.tsx
@@ -40,7 +40,7 @@ const Index = ({ children }: Props) => {
    </svg>
 </button>
 
-<aside id="sidebar-multi-level-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+<aside  id="sidebar-multi-level-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
    <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
       <ul className="space-y-2 font-medium">
           <li>
